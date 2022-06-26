f9b7bb0a590bee997cae552bd70795784959abb5
Roiya1 <107904523+Roiya1@users.noreply.github.com>
2022-06-26 08:16:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,29 +20,29 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is sᏞ ᏒᎾᎥᎽᎪ🔥
+My Creator is Roshan Induwara😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94765415687'] //Owner number in aive msg
+global.premium = ['+94765415687'] //Owner Number info
+global.ownernomer = '+94765415687' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = '😎®∾ᏒᎾᎥᎽᎪ™シ' //Owner Name
+global.botname = ' ☬sᏞ༒ᏒᎾᎥᎽᎪ' //Bot Name
 
-global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
-global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
+global.button = '🤭ටච් එකක් දාන්න අයියෝ📍' //Costomize A Button Name In Alive Message
+global.btnurl = '🙂🔥https://chat.whatsapp.com/KFp7h2ttd6R6gY0xvxDiHn😑' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
-global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
+global.packname = '☬sᏞ༒ᏒᎾᎥᎽᎪ࿐' //Bot Pacage Name
+global.author = '😎®∾ᏒᎾᎥᎽᎪ™シ' //Author Name
+global.prefa = ['$','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor In Menu!
 
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://www.facebook.com/profile.php?id=100076248744090' //Fb Account!
 global.footer = '©_Kumuthu.' // DO NOT CHANGE!
 
 global.mess = { // Error Mesages
